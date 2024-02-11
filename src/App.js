8c32c0d0feeb2403ecabca7521322043d7729fb1
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2024-02-11 16:07:41 +0100

    test 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useEffect, useMemo, useRef, useState } from "react";
+
 import './App.css';
+import { BrowserMultiFormatReader } from '@zxing/library';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <BarcodeScanner onResult={(data) => {
+                    console.log('result', data);
+                    setData(data);
+                }} />
+
+                <p>
+                    {data ? data.getText() : 'Scanning...'}
+                </p>
+            </header>
+        </div>
+    );
 }
 
+export const BarcodeScanner = ({
+                                   onResult = () => {
+                                   }, onError = () => {
+    },
+                               }) => {
+    const { ref } = useZxing({ onResult, onError });
+    return <video ref={ref}/>;
+};
+
+const useZxing = ({
+                      constraints = { audio: false, video: { facingMode: 'environment', }, },
+                      hints,
+                      timeBetweenDecodingAttempts = 300,
+                      onResult = () => {
+                      },
+                      onError = () => {
+                      },
+                  }) => {
+    const ref = useRef(null);
+
+    const reader = useMemo(() => {
+        const instance = new BrowserMultiFormatReader(hints);
+        instance.timeBetweenDecodingAttempts = timeBetweenDecodingAttempts;
+        return instance;
+    }, [hints, timeBetweenDecodingAttempts]);
+
+    useEffect(() => {
+        if (!ref.current) return;
+        reader.decodeFromConstraints(constraints, ref.current, (result, error) => {
+            if (result) onResult(result);
+            if (error) onError(error);
+        });
+        return () => {
+            reader.reset();
+        };
+    }, [ref, reader]);
+
+    return { ref };
+};
 export default App;
